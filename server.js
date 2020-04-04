abfae5d506a7d17f8b86019d88116c0adb799f5d
skvisual <southkvisual@gmail.com>
2020-04-04 17:05:25 -0500

    Moved questions out of start(), Moved prompts inside start()

--- server.js
+++ server.js
@@ -22,58 +22,66 @@ connection.connect(function(err) {
   start();
 });
 
+const questions = [
+  {
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: 
+    [
+      'View Department',
+      'View Roles',
+      'View Employees',
+      'Add Department',
+      'Add Role',
+      'Add Employee',
+      'Update Employee',
+      'EXIT',
+    ],
+    name: 'task'
+  },
+]
+
 function start(){
-
-  const questions = [
-    {
-      type: 'list',
-      message: 'What would you like to do?',
-      choices: 
-      [
-        'View Department',
-        'View Roles',
-        'View Employees',
-        'Add Department',
-        'Add Role',
-        'Add Employee',
-        'Update Employee',
-        'EXIT',
-      ],
-      name: 'task'
-    },
-  ]
-}
-
-inquirer
-.prompt(questions)
-.then(function(answers){
+  inquirer
+  .prompt(questions)
+  .then(function(answers){
   
   switch(answers.task){
     case 'View Department':
       viewDepartment();
     break
+
     case 'View Roles':
       viewRole();
     break
+
     case 'View Employees':
       viewEmployee();
     break
+
     case 'Add Department':
       addDepartment();
     break
+
     case 'Add Role':
       addRole();
     break
+
     case 'Add Employee':
       addEmployee();
     break
+
     case 'Update Employee':
       updateEmployee();
     break
+
     case 'EXIT':
       return 
   }
 })
+}
+
+
 
 function addDepartment() {
   console.log('Creating new department')
@@ -140,4 +148,3 @@ function updateEmployee(){
   start();
 }
 
-    
