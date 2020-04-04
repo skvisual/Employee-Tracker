8d589e0ef8b3fb5a669c7af9cd40a5de55cd33fb
skvisual <southkvisual@gmail.com>
2020-04-04 16:59:28 -0500

    Created functions and prompts

--- cms_db.sql
+++ cms_db.sql
@@ -19,6 +19,7 @@ CREATE TABLE role (
 CREATE TABLE employee (
   id INTEGER(10) AUTO_INCREMENT NOT NULL,
   firstName VARCHAR(30) NOT NULL,
+  title VARCHAR(30) NOT NULL,
   lastName VARCHAR(30) NOT NULL,
   roleId INTEGER(30) NOT NULL,
   managerId INTEGER(30),
@@ -27,16 +28,16 @@ CREATE TABLE employee (
 
 -- STARTING INFO FOR DEPARTMENT
 INSERT INTO department (id, name)
-VALUES (1, 'Finance'), (2 'Executive'),(3, 'IT'),(4, 'Admin');
+VALUES (1, 'Finance'),(2, 'Executive'),(3, 'IT'),(4, 'Admin');
 
 -- STARTING INFOR FOR ROLES
-INSERT INTO roles (id, title, salary, department_id)
-VALUES (1, 'Accountant', 75000, 1),(2, 'Business Analyst', 60000, 2),(3, 'Data Engineer', 70000, 3),(4, 'Executive Director', 2)
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, 'Accountant', 75000, 1),(2, 'Payroll Admin', 60000, 4),(3, 'Data Engineer', 70000, 3),(4, 'Executive Director', 2);
 
 
 -- STARTING INFO FOR EMPLOYEES
 INSERT INTO employee (firstName, lastName, roleId, managerId)
-VALUES ('Andrew', 'Appleseed', 10, 100), ('Barry', 'White', 20, 200), ('Charlie', 'Chaplin', 30, 300);
+VALUES ('Andrew', 'Appleseed', 1, 100), ('Barry', 'White', 4, 200), ('Charlie', 'Chaplin', 3, 300), ('Joe', 'Exotic', 2, null);
 
 
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
-    port: 3306,
+    port: process.env.PORT || 3306,
 
     // Your username
     user: "root",
@@ -19,33 +19,125 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  createDepartment();
+  start();
 });
 
+function start(){
 
-function createDepartment() {
+  const questions = [
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: 
+      [
+        'View Department',
+        'View Roles',
+        'View Employees',
+        'Add Department',
+        'Add Role',
+        'Add Employee',
+        'Update Employee',
+        'EXIT',
+      ],
+      name: 'task'
+    },
+  ]
+}
+
+inquirer
+.prompt(questions)
+.then(function(answers){
+  
+  switch(answers.task){
+    case 'View Department':
+      viewDepartment();
+    break
+    case 'View Roles':
+      viewRole();
+    break
+    case 'View Employees':
+      viewEmployee();
+    break
+    case 'Add Department':
+      addDepartment();
+    break
+    case 'Add Role':
+      addRole();
+    break
+    case 'Add Employee':
+      addEmployee();
+    break
+    case 'Update Employee':
+      updateEmployee();
+    break
+    case 'EXIT':
+      return 
+  }
+})
+
+function addDepartment() {
   console.log('Creating new department')
 
   var query = connection.query(
     'INSERT INTO department SET ? WHERE ?',
-    function(err) {if (err) throw err;}
-  );
+    function(err) {if (err) throw err;
+      console.log(response.affectedRows + 'department created \n')
+    },
+  )
+  start();
+}
 
-function createRole(){
+function addRole(){
   console.log('Creating a new role');
   var query = connection.query(
-    'UPDATE role SET ? WHERE ?'
+    'INSERT INTO role SET ? WHERE ?',
+    function(err) {if (err) throw err;
+      console.log(response.affectedRows + 'role created \n')
+    },
   )
+  start();
 }
 
-function createEmployee(){
+function addEmployee(){
   console.log('Creating a new employee');
   var query = connection.query(
-    'UPDATE employee SET ? WHERE ?'
+    'INSERT INTO employee SET ? WHERE ?',
+    function(err) {if (err) throw err;
+      console.log(response.affectedRows + 'Employee created \n')
+    },
   )
+  start();
 }
 
-function readDepartment(){
-  
+function viewDepartment(){
+  console.log('Selecting all departments \n');
+  var query = connection.query(
+    'SELECT * FROM department',
+    function(err) {if (err) throw err;
+      console.getTable(response)     
+    },
+  )
+  start();
 }
-  
+
+function viewRole(){
+  console.log('Selecting all roles \n');
+  var query = connection.query(
+    'SELECT * FROM role',
+    function(err) {if (err) throw err;
+      console.getTable(response)
+  })
+  start();
+}
+
+function updateEmployee(){
+  console.log('Updating employee info \n')
+  var query = connection.query(
+    'UPDATE employee SET ? WHERE ?',
+    function(err) {if (err) throw err;
+      console.getTable(response.affectedRows + 'Employee information updated \n')
+  })
+  start();
+}
+
+    
